979a102af3a78c0762efaef481ed2707a8e8b9b5
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-02-16 13:00:04 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='SocialCommons',
+    version='0.1dev',
+    packages=['socialcommons',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
+
+
