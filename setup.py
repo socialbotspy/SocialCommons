d26939c02fa89a99dc67ab5abb8a75f690aa701d
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-07-16 08:01:35 +0530

    added few more props to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import setuptools
 from distutils.core import setup
 
 with open("requirements.txt") as f:
     dependencies = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='SocialCommons',
     version='0.1dev',
+    author="Ishan Dutta",
+    author_email="ishandutta2007@gmail.com",
+    description="Common library for all XPy bots under socialbotspy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=['socialcommons',],
     install_requires=dependencies,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
